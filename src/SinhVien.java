7221f02be405755eaa1cbac4abdaaa34801be568
HuongLan <tnphonglan@gmail.com>
2024-07-08 18:57:02 +0700

    Add folder src

---
+++ src/SinhVien.java
@@ -0,0 +1,21 @@
+public class SinhVien {
+    String Khoa = "Địa Lý";  //Biến toàn cục
+    public static String Lop = "GIS";
+    public void getMaSV() {
+        String MaSV = "001";      // Biến cục bộ
+        System.out.println("MaSV la: " + MaSV);
+    }
+    public void getKhoa(){
+        System.out.println("Khoa: " + Khoa);
+    }
+    public static void main(String[] args) {
+        SinhVien masv = new SinhVien();
+        masv.getMaSV();
+        SinhVien khoa = new SinhVien();
+        khoa.getKhoa();
+        System.out.println("Lớp: " + Lop);
+        System.out.println( "Tên: " + ThongTin.Ten);
+        System.out.println( "Tuổi: " + ThongTin.Tuoi);
+        System.out.println( "Địa chỉ: " + ThongTin.DiaChi);
+    }
+}
---
+++ src/ThongTin.java
@@ -0,0 +1,5 @@
+public class ThongTin {
+    public static String Ten = "Hương Lan";
+    public static int Tuoi = 19;
+    public static String DiaChi = "Hồ Chí Minh";
+}

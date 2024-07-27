1ee3b098700213c5f3e3f41d1f5eedec97ba0d64
HuongLan <tnphonglan@gmail.com>
2024-07-27 10:02:23 +0700

    update_BT_javabasic5

--- src/BT_javabasic5/NhanVien.java
+++ src/BT_javabasic5/NhanVien.java
@@ -1,17 +1,54 @@
 package BT_javabasic5;
 
+import java.util.ArrayList;
+
 public class NhanVien {
     String name;
     int age;
     String address;
 
-    public NhanVien(String name, int age, String address){
+    public NhanVien(String name, int age, String address) {
         this.name = name;
         this.age = age;
         this.address = address;
     }
-NhanVien nv1 = new NhanVien("Lan", 33, "HCM")
+
+    public String getName() {
+        return name;
+
+    }
+
+    public int getAge() {
+        return age;
+
+    }
+
+    public String getAddress() {
+        return address;
+
+    }
+
+    public void printInfo() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+    }
+
+    public static void main(String[] args) {
+
+        NhanVien nv1 = new NhanVien("Lan", 33, "HCM");
+        NhanVien nv2 = new NhanVien("Nam", 35, "HN");
+
+        ArrayList<NhanVien> ListNhanVien = new ArrayList<>();
+        ListNhanVien.add(nv1);
+        ListNhanVien.add(nv2);
+
+        for (NhanVien nv : ListNhanVien){
+            System.out.println("***********");
+            nv.printInfo();
+        }
 
 
+    }
 }
 
